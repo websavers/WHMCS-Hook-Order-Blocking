316ccf5cfa492efc7f1c6247ce54aeb2daa11f74
Jordan Schelew <jordan@websavers.ca>
2025-08-30 18:02:24 -0300

    Init
    With options to:
    - Block orders for those with unverified email addresses
    - Block orders from clients assigned to a specific client group

---
+++ includes/hooks/orderBlocking.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Prevent orders from clients with an unverified e-mail
+ *
+ * @author     WHMCS Josh Q <support@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Ltd. All Rights Reserved.
+ * @link       https://www.whmcs.com/
+ * @reference  https://whmcs.community/topic/339228-prevent-orders-from-clients-with-an-unverified-e-mail-address/
+ * @contributor websavers
+ */
+
+use WHMCS\Database\Capsule;
+
+if (!defined("WHMCS"))
+        die("This file cannot be accessed directly");
+
+# Allow clients with unverified e-mails to place orders?
+define("BLOCK_UNVERIFIED_EMAILS", true);
+# If brand new client, force going to registration page first and verifying email (extra steps)
+define("BLOCK_IF_NOT_REGISTERED", false);
+# Block based on client group - if admin has blocked the client from placing orders, like if suspected of fraud
+define("BLOCK_CLIENT_GROUP", 'Suspicious');
+
+add_hook("ShoppingCartValidateCheckout", 1, function($vars){
+    $client = Menu::context("client");
+    if (is_null($client)) {
+        if (BLOCK_UNVERIFIED_EMAILS && BLOCK_IF_NOT_REGISTERED){
+            return array("You must <a href='/register.php'>register an account</a> and verify your e-mail before you can place an order.");
+        }
+    }
+    else{ //Client Exists
+        if (BLOCK_UNVERIFIED_EMAILS && $client->isEmailAddressVerified()==false) {
+                return array("You must verify  your e-mail address before you can checkout.");
+        }
+        if (BLOCK_CLIENT_GROUP && $vars['clientid'] > 0){
+            $clientgroup = Capsule::table('tblclients')
+                ->join('tblclientgroups', 'tblclients.groupid', '=', 'tblclientgroups.id')
+                ->where('tblclients.id', $vars['clientid'])
+                ->value('groupname');
+
+            if (BLOCK_CLIENT_GROUP == $clientgroup){
+                return array("Your account has been blocked from placing new orders. To appeal, please <a href='submitticket.php?step=2&deptid=1'>open a ticket</a>.");
+            }
+        }
+    }
+});
