2f7c02f07ef1f847f0c5f1817f230c69c503a7bd
Jordan Schelew <jordan@websavers.ca>
2025-11-14 09:18:24 -0400

    Add fraud management improvements

--- includes/hooks/orderBlocking.php
+++ includes/hooks/orderBlocking.php
@@ -1,12 +1,16 @@
 <?php
 /*
- * Prevent orders from clients with an unverified e-mail
+ * With this module you can:
+ * 1. Prevent new orders from clients with an unverified e-mail
+ * 2. Force registration before order
+ * 3. Prevent new orders from clients in a Group (default: Suspicious -- must be created by admin)
+ * 4. Prevent sending order confirmation email when order marked Fraud
+ * 5. Change client group or status when order marked Fraud
  *
- * @author     WHMCS Josh Q <support@whmcs.com>
- * @copyright  Copyright (c) WHMCS Ltd. All Rights Reserved.
- * @link       https://www.whmcs.com/
+ * @author     Websavers Inc
+ * @link       https://www.websavers.ca
  * @reference  https://whmcs.community/topic/339228-prevent-orders-from-clients-with-an-unverified-e-mail-address/
- * @contributor websavers
+ * @contributor WHMCS
  */
 
 use WHMCS\Database\Capsule;
@@ -20,6 +24,10 @@ define("BLOCK_UNVERIFIED_EMAILS", false);
 define("BLOCK_IF_NOT_REGISTERED", false);
 # Block based on client group - if admin has blocked the client from placing orders, like if suspected of fraud
 define("BLOCK_CLIENT_GROUP", 'Suspicious');
+# This controls whether to send the notification that the order has been held for fraud. You must create an email template called 'Order Fraudulent'
+define("FRAUD_SEND_EMAIL", true);
+# For fraud orders, close client, or set group as suspicious? Note: close will only happen if there's no active products/domains
+define("FRAUD_CLOSE_OR_GROUP", 'group'); //options: 'close', 'group', false
 
 add_hook("ShoppingCartValidateCheckout", 1, function($vars){
     $client = Menu::context("client");
@@ -46,4 +54,78 @@ add_hook("ShoppingCartValidateCheckout", 1, function($vars){
             }
         }
     }
+});
+
+/**
+ * Fraud Handling Improvements.
+ * Block sending of order confirmation email for fraudulent orders
+ * Then send fraud detected email and adjust Client
+ */
+ 
+add_hook('EmailPreSend', 1, function($vars){
+		
+	if ($vars['messagename'] === 'Order Confirmation') {
+		
+		$orderid = $vars['relid'];
+		$response = localAPI('GetOrders', array('id' => $orderid));
+		
+		if ($response['result'] === 'success'){
+			if ($response['orders']['order']['0']['status'] === 'Fraud'){ //can only be 1 order thanks to relid above
+				//logActivity("Fraud Order: " . $response['orders']['order']['0']['id']); ///DEBUG
+				return array('abortsend' => true); //waboom
+			}
+		}
+		
+	}
+		
+});
+
+add_hook('FraudCheckFailed', 1, function($vars){
+		
+    $client_id = $vars['clientdetails']['id'];
+
+    // Send fraud notification email
+    if (FRAUD_SEND_EMAIL){
+        $results = localAPI('SendEmail', array(
+            'messagename' => 'Order Fraudulent',
+            'id' => $client_id,
+        ));
+    }
+
+    switch(FRAUD_CLOSE_OR_GROUP){
+
+        case 'close':
+
+            $num_products = Capsule::table('tblhosting')
+                ->where('userid', $client_id)
+                ->whereIn('domainstatus', ['Active', 'Suspended'])
+                ->count();
+
+            $num_domains = Capsule::table('tbldomains')
+                ->where('userid', $client_id)
+                ->whereIn('status', ['Active', 'Pending Transfer'])
+                ->count();
+
+            if ($num_products == 0 && $num_domains == 0){
+                localAPI('CloseClient', array('clientid' => $client_id));
+            }
+
+            break;
+
+        case 'group':
+
+            $group_id = Capsule::table('tblclientgroups')->where('groupname', BLOCK_CLIENT_GROUP)->value('id');
+            
+            localAPI('UpdateClient', array('groupid' => $group_id));
+
+            break;
+
+        case false:
+        default: 
+
+            break;
+            
+    }
+			
+
 });
